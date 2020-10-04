dd93901621a95e8a4c437866cdd32a3493d2c113
Gabriel Araujo <gabriel227sa@gmail.com>
2020-10-04 20:02:50 -0300

    Starting client

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,12 +43,19 @@ namespace API
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                    c.RoutePrefix = "";
                 });
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy =>
+                policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("https://localhost:4200")
+            );
 
             app.UseAuthorization();
 
